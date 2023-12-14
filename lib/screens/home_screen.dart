6a90ad1d38cdede285348297ebde783203bd14e3
ahmed <Ahmedabdallah11186@gmail.com>
2023-12-14 16:07:09 +0200

    ..

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,6 @@ import 'cart_screen.dart';
 import 'favorite_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-
   const HomeScreen({super.key});
 
   static const route = 'Home_Screen';
@@ -21,8 +20,11 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         shadowColor: Colors.grey,
         centerTitle: true,
-        title:
-            Image.asset('assets/images/x.png', alignment: Alignment.center,height: 50,),
+        title: Image.asset(
+          'assets/images/x.png',
+          alignment: Alignment.center,
+          height: 50,
+        ),
         actions: [
           IconButton(
             onPressed: () {
@@ -94,114 +96,127 @@ class HomeScreen extends StatelessWidget {
                   ])
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 10),
-          SizedBox(
-            height: 80,
-            child: ListView(scrollDirection: Axis.horizontal, children: [
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Row(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 80,
+                child: ListView(scrollDirection: Axis.horizontal, children: [
+                  Column(
                     children: [
-                      InkWell(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (_)=> const CategoryScreen()));
-                        },
-                        child: ClipOval(
-                          child: Image.network(
-                            'https://english.cdn.zeenews.com/sites/default/files/2022/02/20/1016233-iphone14.jpg',
-                            width: 70,
-                            height: 70,
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => const CategoryScreen()));
+                            },
+                            child: ClipOval(
+                              child: Image.network(
+                                'https://english.cdn.zeenews.com/sites/default/files/2022/02/20/1016233-iphone14.jpg',
+                                width: 70,
+                                height: 70,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      ClipOval(
-                        child: Image.network(
-                          'https://th.bing.com/th/id/R.d79e5ca7ac6e51ce0ac2e26365e87766?rik=mwLpSP%2fescGqMA&pid=ImgRaw&r=0',
-                          width: 70,
-                          height: 70,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      ClipOval(
-                        child: Image.network(
-                          'https://th.bing.com/th/id/R.a0bb639c702b8187846d5f8d4ef0c39a?rik=8hN6lRueL5xdqw&pid=ImgRaw&r=0',
-                          width: 70,
-                          height: 70,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      ClipOval(
-                        child: Image.network(
-                          'https://picsum.photos/200',
-                          width: 70,
-                          height: 70,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      ClipOval(
-                        child: Image.network(
-                          'https://picsum.photos/200',
-                          width: 70,
-                          height: 70,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      ClipOval(
-                        child: Image.network(
-                          'https://picsum.photos/200',
-                          width: 70,
-                          height: 70,
-                        ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ClipOval(
+                            child: Image.network(
+                              'https://th.bing.com/th/id/R.d79e5ca7ac6e51ce0ac2e26365e87766?rik=mwLpSP%2fescGqMA&pid=ImgRaw&r=0',
+                              width: 70,
+                              height: 70,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ClipOval(
+                            child: Image.network(
+                              'https://th.bing.com/th/id/R.a0bb639c702b8187846d5f8d4ef0c39a?rik=8hN6lRueL5xdqw&pid=ImgRaw&r=0',
+                              width: 70,
+                              height: 70,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ClipOval(
+                            child: Image.network(
+                              'https://picsum.photos/200',
+                              width: 70,
+                              height: 70,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ClipOval(
+                            child: Image.network(
+                              'https://picsum.photos/200',
+                              width: 70,
+                              height: 70,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ClipOval(
+                            child: Image.network(
+                              'https://picsum.photos/200',
+                              width: 70,
+                              height: 70,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ]),
               ),
-            ]),
-          ),
-          const SizedBox(height: 10),
-          const Divider(thickness: 3),
-          Expanded(
-            child: FutureBuilder<List<Product>?> (
-                future: DioNetwork().getProducts(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (snapshot.data == null) {
-                    return const Center(child: Text('something went wrong !'));
-                  }
+              const SizedBox(height: 10),
+              const Divider(thickness: 3),
+              Expanded(
+                child: FutureBuilder<List<Product>?>(
+                    future: DioNetwork().getProducts(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      if (snapshot.data == null) {
+                        return const Center(
+                            child: Text('something went wrong !'));
+                      }
 
-                  if (snapshot.data!.isEmpty) {
-                    return const Center(child: Text('no products available right now'));
-                  }
+                      if (snapshot.data!.isEmpty) {
+                        return const Center(
+                            child: Text('no products available right now'));
+                      }
 
-                  return GridView.builder(
-                      itemCount: snapshot.data!.length,
-                      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 300, mainAxisExtent: 300, crossAxisSpacing: 5),
-                      itemBuilder: (_, index) {
-                        return ProductCard(snapshot.data![index]);
-                      });
-                }),
+                      return GridView.builder(
+                          itemCount: snapshot.data!.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithMaxCrossAxisExtent(
+                                  maxCrossAxisExtent: 300,
+                                  mainAxisExtent: 300,
+                                  crossAxisSpacing: 5),
+                          itemBuilder: (_, index) {
+                            return ProductCard(snapshot.data![index]);
+                          });
+                    }),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
       floatingActionButton: SpeedDial(
         icon: Icons.card_travel_outlined,
